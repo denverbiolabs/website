ea7946e2468bca56d34a379738da9e561a76060c
Devin Gray <devin.gray@colorado.edu>
2017-12-07 07:00:26 -0700

    deleted package-lock folder, fixed module not compliling problem

--- posts-app-client/src/containers/Login.js
+++ posts-app-client/src/containers/Login.js
@@ -10,22 +10,6 @@ import {
 
 
 
-login(email, password) {
-  const userPool = new CognitoUserPool({
-    UserPoolId: config.cognito.USER_POOL_ID,
-    ClientId: config.cognito.APP_CLIENT_ID
-  });
-  const user = new CognitoUser({ Username: email, Pool: userPool });
-  const authenticationData = { Username: email, Password: password };
-  const authenticationDetails = new AuthenticationDetails(authenticationData);
-
-  return new Promise((resolve, reject) =>
-    user.authenticateUser(authenticationDetails, {
-      onSuccess: result => resolve(),
-      onFailure: err => reject(err)
-    })
-  );
-}
 
 
 
@@ -33,11 +17,17 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
 
+
+
+
+
     this.state = {
       email: "",
       password: ""
     };
   }
+
+
 
 
 
@@ -52,6 +42,25 @@ export default class Login extends Component {
     });
   }
 
+
+      login(email, password) {
+        const userPool = new CognitoUserPool({
+          UserPoolId: config.cognito.USER_POOL_ID,
+          ClientId: config.cognito.APP_CLIENT_ID
+        });
+        const user = new CognitoUser({ Username: email, Pool: userPool });
+        const authenticationData = { Username: email, Password: password };
+        const authenticationDetails = new AuthenticationDetails(authenticationData);
+
+        return new Promise((resolve, reject) =>
+          user.authenticateUser(authenticationDetails, {
+            onSuccess: result => resolve(),
+            onFailure: err => reject(err)
+          })
+        );
+      }
+
+
   handleSubmit = async event => {
     event.preventDefault();
 
@@ -62,6 +71,8 @@ export default class Login extends Component {
       alert(e);
     }
   }
+
+
 
   render() {
     return (
