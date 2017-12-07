1514bd12470d2695dffda64d162cda5c6f05152b
Devin Gray <devin.gray@colorado.edu>
2017-12-07 03:45:34 -0700

    back-end tested and deployed

--- post-app-api/.webpack/service/list.js
+++ post-app-api/.webpack/service/list.js
@@ -92,22 +92,26 @@ var main = exports.main = function () {
           case 0:
             params = {
               TableName: "biolabs-posts",
-              // 'Key' defines the partition key and sort key of the item to be removed
-              // - 'userId': Identity Pool identity id of the authenticated user
-              // - 'noteId': path parameter
-              Key: {
-                userId: event.requestContext.identity.cognitoIdentityId,
-                postId: event.pathParameters.id
+              // 'KeyConditionExpression' defines the condition for the query
+              // - 'userId = :userId': only return items with matching 'userId'
+              //   partition key
+              // 'ExpressionAttributeValues' defines the value in the condition
+              // - ':userId': defines 'userId' to be Identity Pool identity id
+              //   of the authenticated user
+              KeyConditionExpression: "userId = :userId",
+              ExpressionAttributeValues: {
+                ":userId": event.requestContext.identity.cognitoIdentityId
               }
             };
             _context.prev = 1;
             _context.next = 4;
-            return dynamoDbLib.call("delete", params);
+            return dynamoDbLib.call("query", params);
 
           case 4:
             result = _context.sent;
 
-            callback(null, (0, _responseLib.success)({ status: true }));
+            // Return the matching list of items in response body
+            callback(null, (0, _responseLib.success)(result.Items));
             _context.next = 11;
             break;
 
